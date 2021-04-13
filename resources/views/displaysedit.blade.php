dce9ff9e68d94768285461017f23a6e21ca8b84e
hiroyukimurasawa <[hiromra1234@gmail.com]>
2021-04-13 03:42:46 +0000

    comment

--- resources/views/displaysedit.blade.php
+++ resources/views/displaysedit.blade.php
@@ -15,8 +15,7 @@
         </div>
         @endcan
         <div class="form-group">
-            <p style="height:200px;">
-            <label for="display_img"><img id="img_prv" src="/storage/{{$display->id}}.jpg" width="640" height="960"></label>
+            <label for="display_img"><img id="img_prv" src="/storage/{{$display->id}}.jpg" width="320" height="480"></label>
         @can('editor')
             <input type="file" id="display_img" name="display_img" class="form-control " value="{{$display->display_img}}">
         @endcan
